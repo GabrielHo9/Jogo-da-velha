7e002cc9f3e36fe4387930a3f36e5f25b1e004bc
GabrielHo9 <121255247+GabrielHo9@users.noreply.github.com>
2024-11-25 20:59:59 -0300

    Interface atualizada:)

--- jogodavelha.cpp
+++ jogodavelha.cpp
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 //fazer um jogo da velha funcional com tela inicial,
 
 //variaveis
@@ -21,6 +24,30 @@ void iniciarT() {
     printf("Bem-vindo, Novo Jogador!\n");
     printf("Digite seu nome para sua identificação: ");
     scanf("%s", nome);
+
+ while (1) {
+        printf("\nEscolha como prosseguir: \n");
+        printf("Digite as seguintes opções:\n|| Novo | rank | cred | sair ||\n");
+        scanf("%s", escolha);
+
+        if (strcmp(escolha, "Novo") == 0) {
+            printf("Iniciando um novo jogo...\n");
+            iniciarT();
+
+
+        } else if (strcmp(escolha, "rank") == 0) {
+            printf("Mostrando a tabela de ranking...\n");
+    
+        } else if (strcmp(escolha, "cred") == 0) {
+            printf("Exibindo os créditos...\n");
+            printf("Desenvolvido por: Gabriel Pereira Ho\nPedro Henrique de Macedo Oliveira\nArthur Lobo Freitas\nJoão Vitor Amaral");
+        } else if (strcmp(escolha, "sair") == 0) {
+            printf("Saindo do jogo. Até logo, %s!\n", nome);
+            break;
+        } else {
+            printf("Opção inválida. Tente novamente.\n");
+        }
+    }
 
 
 
