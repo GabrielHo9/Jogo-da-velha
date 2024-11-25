6abdc267a26e6f567e278d3a4d8004292660b4c6
GabrielHo9 <121255247+GabrielHo9@users.noreply.github.com>
2024-11-25 20:42:24 -0300

    Criando o Tabuleiro
    Criando a base da matriz

---
+++ jogodavelha.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+//fazer um jogo da velha funcional com tela inicial,
+
+//variaveis
+char tabuleiro[3][3];
+char jogador1 = 'X';
+
+//void do tabuleiro
+void iniciarT() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            tabuleiro[i][j] = ' ';
+        }
+    }
+      
+}
+ int main(){
+
+
+
+
+return 0;
+}
+ 
+  
+
