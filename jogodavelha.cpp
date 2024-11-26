810cc938973affa17b204499e675487238b83505
GabrielHo9 <121255247+GabrielHo9@users.noreply.github.com>
2024-11-25 21:08:29 -0300

    mudancas substanciais + tabuleiro aparece no terminal

--- jogodavelha.cpp
+++ jogodavelha.cpp
@@ -17,6 +17,28 @@ void iniciarT() {
     }
       
 }
+void mostrarT() {
+    printf("\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf(" %c ", tabuleiro[i][j]);
+            if (j < 2) printf("|");
+        }
+        printf("\n");
+        if (i < 2) printf("---+---+---\n");
+    }
+    printf("\n");
+}
+int jogada(int linha, int coluna) {
+    if (linha < 1 || linha > 3 || coluna < 1 || coluna > 3 || tabuleiro[linha - 1][coluna - 1] != ' ') {
+        return 0; //inválida
+    }
+    tabuleiro[linha - 1][coluna - 1] = jogador1;
+    return 1; //válida
+}
+
+
+//main
  int main(){
     int linha, coluna;
     char nome[50], escolha[265];
@@ -30,10 +52,15 @@ void iniciarT() {
         printf("Digite as seguintes opções:\n|| Novo | rank | cred | sair ||\n");
         scanf("%s", escolha);
 
-        if (strcmp(escolha, "Novo") == 0) {
+        if (strcmp(escolha, "novo") == 0) {
             printf("Iniciando um novo jogo...\n");
             iniciarT();
-
+            
+            while (1) {
+                mostrarT();
+                printf("Jogador %c, faça sua jogada (linha e coluna): ", jogador1);
+                scanf("%d %d", &linha, &coluna);
+            }
 
         } else if (strcmp(escolha, "rank") == 0) {
             printf("Mostrando a tabela de ranking...\n");
